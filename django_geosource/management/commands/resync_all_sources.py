28157f5a5653626c112123199f9f33e1443e1d95
Nahuel ANGELINETTI <nahuel.angelinetti@makina-corpus.com>
2019-09-05 16:13:55 +0200

    Add management command to resync all sources

---
+++ django_geosource/management/commands/resync_all_sources.py
@@ -0,0 +1,14 @@
+from django.core.management import BaseCommand
+
+from django_geosource.models import Source
+
+
+class Command(BaseCommand):
+    help = 'Launch resync of all sources availables'
+
+    def add_arguments(self, parser):
+        parser.add_argument('--force', dest='force', action='store_true')
+
+    def handle(self, *args, **options):
+        for source in Source.objects.all():
+            source.run_async_method('refresh_data', force=options['force'])
