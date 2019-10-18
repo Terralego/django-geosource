58cdae31d7b75ea602c512b1389f9916b1544f48
Nahuel ANGELINETTI <nahuel.angelinetti@makina-corpus.com>
2019-10-18 11:43:00 +0200

    Setup travis pipeline

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM makinacorpus/geodjango:bionic-3.7
+
+RUN mkdir -p /code/src
+COPY . /code/src
+WORKDIR /code/src
+
+RUN useradd -ms /bin/bash django
+RUN chown -R django:django /code
+
+USER django
+
+RUN python3.7 -m venv /code/venv
+RUN  /code/venv/bin/pip install --no-cache-dir pip setuptools wheel -U
+
+# Install dev requirements
+RUN /code/venv/bin/pip3 install --no-cache-dir -e . -U
--- setup.py
+++ setup.py
@@ -1,36 +1,51 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
-from setuptools import find_packages, setup
+import os
+from setuptools import setup, find_packages
 
+HERE = os.path.abspath(os.path.dirname(__file__))
 
-def read(fname):
-    return open(fname).read()
+README = open(os.path.join(HERE, 'README.md')).read()
+CHANGES = open(os.path.join(HERE, 'CHANGES.md')).read()
 
+test_require = [
+    'factory-boy',
+    'flake8',
+    'coverage',
+]
 
 setup(
-    name="django_geosource",
-    version="0.1",
+    name='django-geosource',
+    version=open(os.path.join(HERE, 'django_geosource', 'VERSION.md')).read().strip(),
+    include_package_data=True,
     author="Makina Corpus",
     author_email="terralego-pypi@makina-corpus.com",
-    description="Django GeoSource",
-    url="https://github.com/Terralego/django-geosource",
-    packages=find_packages(
-        exclude=["*.tests", ]
-    ),
-    include_package_data=True,
-    long_description=read("README.md"),
+    description='Django geographic store and vector tile generation',
+    long_description=README + '\n\n' + CHANGES,
+    description_content_type="text/markdown",
     long_description_content_type="text/markdown",
+    packages=find_packages(),
+    url='https://github.com/Terralego/django-geosource.git',
     classifiers=[
-        "Programming Language :: Python",
-        "Topic :: Software Development",
-        "OSI Approved :: MIT License",
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     install_requires=[
         "Django==2.2.5",
         "djangorestframework>=3.8,<3.9",
         "django-polymorphic>=2.0,<2.1",
         "celery>=4.3,<4.4",
+        "psycopg2>=2.7",
         "Fiona>=1.8,<1.9",
     ],
+    tests_require=test_require,
 )
