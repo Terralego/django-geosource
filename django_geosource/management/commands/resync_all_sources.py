2a418f0b975cb25b0212b376649feb90f2f2e273
Jeremie Pardou <571533+jrmi@users.noreply.github.com>
2020-02-19 09:49:31 +0100

    Add option to sync sources to have more control

--- django_geosource/management/commands/resync_all_sources.py
+++ django_geosource/management/commands/resync_all_sources.py
@@ -4,11 +4,41 @@ from django_geosource.models import Source
 
 
 class Command(BaseCommand):
-    help = "Launch resync of all sources availables"
+    help = "Launch resync of all available sources"
 
     def add_arguments(self, parser):
-        parser.add_argument("--force", dest="force", action="store_true")
+        parser.add_argument(
+            "--force",
+            dest="force",
+            action="store_true",
+            help="Force sync even if already launched",
+        )
+        parser.add_argument(
+            "--ask",
+            dest="ask",
+            action="store_true",
+            help="Ask confirmation before syncing a source",
+        )
+        parser.add_argument(
+            "--sync", dest="sync", action="store_true", help="Run in sync"
+        )
 
     def handle(self, *args, **options):
         for source in Source.objects.all():
-            source.run_async_method("refresh_data", force=options["force"])
+            if options["ask"]:
+                resp = (
+                    input(
+                        f"Do you like to refresh the source {source}<{source.id}>? [y|N]"
+                    )
+                    or "n"
+                )
+            else:
+                resp = "y"
+
+            if resp.lower() == "y":
+                if options["sync"]:
+                    print(f"Refreshing source {source}<{source.id}>...")
+                    source.refresh_data()
+                else:
+                    print(f"Schedule refresh for source {source}<{source.id}>...")
+                    source.run_async_method("refresh_data", force=options["force"])
